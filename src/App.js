3dfbbc5f7b1081ece59408cf60bbe5f791050b0d
jayashibus <jayashibu@gmail.com>
2022-08-31 22:53:05 +0530

    Account verified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Verify from "./component/Verify";
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
-      <Verify />
+      <Verify data={data} />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    staff_pass_id: "STAFF_H123804820G",
+    team_name: "BASS",
+    created_at: "1623772799000",
+  },
+  {
+    staff_pass_id: "MANAGER_T999888420B",
+    team_name: "RUST",
+    created_at: "1623772799000",
+  },
+
+  {
+    staff_pass_id: "BOSS_T000000001P",
+    team_name: "RUST",
+    created_at: "1623872111000",
+  },
+];
