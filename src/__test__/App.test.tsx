29e94bd3daf676dd75df3a1e0047dc28fbef9099
jayashibus <jayashibu@gmail.com>
2022-09-04 18:19:33 +0530

    Unnessary test files removed

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "../App";
-// import { rest } from "msw";
-// import { server } from "./../mocks/server";
-
-// import { getStaffDetails } from "./http/HttpService";
 
 test("renders react app", async () => {
   render(<App />);
